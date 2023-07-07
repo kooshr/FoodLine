f7a7db1af209e29f8a16b2ae542de2acc6cc1034
kooshr <kaushalramesh1@gmail.com>
2023-07-06 22:03:45 -0700

    made images work properly

--- Screens/colors.js
+++ Screens/colors.js
@@ -3,7 +3,7 @@ const colors = {
     search : '#C4DFE6',     //Light Blue
     tint: '#E2E8E4',        //White
     accent: '#CCA78F',      //Light Brown
-    text: '#BF8660'          //Dark Brown
+    text: '#BF8660'         //Dark Brown
 }
 
 export default colors
